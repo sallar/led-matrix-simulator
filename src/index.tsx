e8a64394eb5037c2d0705d708c4dab6c86186c91
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-12-03 12:54:36 +0200

    Transpile to ES5

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+// Polyfills
+import 'core-js/fn/string/repeat';
+import 'core-js/fn/array/fill';
+import 'core-js/fn/object/assign';
+
 import { h, render } from 'preact';
 import Playground from './playground';
 import Symbol from './symbols';
